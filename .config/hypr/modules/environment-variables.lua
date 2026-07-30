f09daa7c20b54696e74205e8224532d71e36c4b6
Aovos <anastasious@gmx.net>
2026-07-30 09:41:53 +0200

    Create environment-variables.lua

---
+++ .config/hypr/modules/environment-variables.lua
@@ -0,0 +1,7 @@
+-- [[ Modul: Umgebungsvariablen - modules/environment-variables.lua ]] --
+
+hl.env("GTK_ICON_THEME", "Papirus-Dark")
+hl.env("XCURSOR_THEME", "McMojave-cursors")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "McMojave-cursors")
+hl.env("HYPRCURSOR_SIZE", "24")
