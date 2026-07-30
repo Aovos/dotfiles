a96bcf10230f83c83acae8159bde116f168db541
Aovos <anastasious@gmx.net>
2026-07-30 10:26:47 +0200

    Create my-programs.lua

---
+++ .config/hypr/modules/my-programs.lua
@@ -0,0 +1,6 @@
+-- [[ Modul: Standard-Programme - modules/my-programs.lua ]] --
+
+-- Globale Variablen für alle Module
+terminal    = "alacritty"
+fileManager = "nemo"
+menu        = "rofi -show drun"
